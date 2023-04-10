0ec4bea9a1c23e22fcf513d04815429b5bfb3947
unknown <haseeb@empirepixel.com>
2023-04-10 23:28:34 +0500

    update

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -47,12 +47,12 @@ const Hero = ({props}) => {
   />
 )}
   return (
-    <section className='bg-[#11031A] relative min-h-[110vh] overflow-hidden md:pb-[10.3125vw]'>
+    <section className='bg-[#11031A] relative min-h-[110vh] overflow-hidden md:pb-[13.3125vw]'>
         <img src={bg} alt="Background Image" className='w-full h-full object-cover top-[93.25px] md:top-[104px] absolute md:left-[41.57px]
          hidden md:block' />
         <img src={mobbg} alt="Background Image" className='w-full object-cover top-[93.25px] h-[706.52px] relative md:left-0 md:hidden block' />
        
-        <video src={backgroundVideo} autoPlay loop muted className='w-full h-[429.5px] object-cover left-[50%] md:w-[70.546875vw] top-[247.56px] md:h-[unset] md:left-[-1vw] md:top-[22vw] absolute 
+        <video src={backgroundVideo} autoPlay loop muted className='w-full h-[429.5px] object-cover left-[50%] md:w-[70.546875vw] top-[247.56px] md:h-[unset] md:left-[-1vw] md:top-[24vw] absolute 
         md:absolute mix-blend-lighten transform translate-x-[-50%] skew-x-[22deg] skew-y-[2deg] md:translate-x-[unset] md:skew-y-[10deg] md:skew-x-[10deg]'/>
 
         <div className='w-[18.671875vw] h-[33.984375vw] absolute right-[-13.046875vw] top-[22.5vw] border border-red-400  homeAboutGradient hidden md:block'>
@@ -69,7 +69,7 @@ const Hero = ({props}) => {
            
             <div className='z_index w-[305px] md:w-[23.828125vw] md:mb-[1.875vw] flex text-center flex-col gap-y-[12px] mb-[32.2px] md:gap-y-[0.9375vw] text-[#8C7998]'>
                <HomePara classes={"z_index text-[#8C7998]"} title={"We are reintroducing the Algorand blockchain to prize-linked staking, which combines low-yield staking with a weekly prize game! Prizes are generated on the interest earned on deposited funds through participation in the Algorand Foundation Governance program. By leveraging this low-risk mechanism, we are able to guarantee a prize pool and an exciting weekly community event!"}/>
-               <BtnGroups props={props} isDepositValid={isDepositValid} ButtonTypes_DEPOSIT={ButtonTypes.DEPOSIT} ButtonTypes_WITHDRAW={ButtonTypes.WITHDRAW} openForm={openForm} classes={"white pr-[130px] mt-[40px]"} />
+               <BtnGroups props={props} isDepositValid={isDepositValid} ButtonTypes_DEPOSIT={ButtonTypes.DEPOSIT} ButtonTypes_WITHDRAW={ButtonTypes.WITHDRAW} openForm={openForm} classes={"white pr-[90px] mt-[40px]"} />
            
             </div>
   
